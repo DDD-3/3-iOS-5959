b6e363db8b6be940a970525f5f8865f1fe78ef6c
devssun <chocochic85@gmail.com>
2019-12-06 13:34:27 +0900

    [INIT] 프로젝트 초기 설정

---
+++ iOS5959/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS5959
+//
+//  Created by 최혜선 on 2019/12/06.
+//  Copyright © 2019 jamie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS5959Tests/iOS5959Tests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS5959Tests.swift
+//  iOS5959Tests
+//
+//  Created by 최혜선 on 2019/12/06.
+//  Copyright © 2019 jamie. All rights reserved.
+//
+
+import XCTest
+@testable import iOS5959
+
+class iOS5959Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
