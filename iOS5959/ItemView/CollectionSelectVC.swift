cb39d5264b60c8326876a7981208c044eac240e3
s1gnature <lamant0486@naver.com>
2020-01-15 17:46:39 +0900

    [ADD] 콜렉션 선택VC

---
+++ iOS5959/ItemView/CollectionSelectVC.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionSelectVC.swift
+//  iOS5959
+//
+//  Created by mong on 2020/01/15.
+//  Copyright © 2020 5959. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionSelectVC: UIViewController{
+    override func viewDidLoad() {
+        
+    }
+}
