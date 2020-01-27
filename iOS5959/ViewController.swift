7ce9d919fd11ba2ef27a88a1aae7f91e60376c32
devssun <chocochic85@gmail.com>
2020-01-27 13:43:39 +0900

    [ADD] NavigationBar Title 수정 (타이틀 누르면 콜렉션 수정 팝업 뜨는 기능 추가)
    - 리스트 버튼 추가
    - 타이틀 누르면 콜렉션 수정 팝업 뜨는 기능 추가

--- iOS5959/ViewController.swift
+++ iOS5959/ViewController.swift
@@ -14,10 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        navigationController?.navigationBar.barTintColor = .white
-        navigationController?.navigationBar.tintColor = .black
-        navigationItem.title = "WishBall"
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "menu"), style: .plain, target: self, action: #selector(touchedSideMenuButton))
+        setNavigationBar()
     }
 
     @objc private func touchedSideMenuButton() {
@@ -26,6 +23,39 @@ class ViewController: UIViewController {
             sideMenu.statusBarEndAlpha = 0
             self.present(sideMenu, animated: true, completion: nil)
         }
+    }
+    
+    @objc private func touchedListToggleButton() {
+        print("리스트 전환")
+    }
+    
+    @objc private func touchedModifyCollection() {
+        print("콜렉션 수정")
+        let modifyCollectionViewController = ModifyCollectionViewController()
+        modifyCollectionViewController.editMode = .modify
+        modifyCollectionViewController.modalPresentationStyle = .fullScreen
+        self.present(modifyCollectionViewController, animated: true, completion: nil)
+    }
+    
+    private func setNavigationBar() {
+        navigationController?.navigationBar.barTintColor = .white
+        navigationController?.navigationBar.tintColor = .black
+        navigationItem.title = "WishBall"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "menu"), style: .plain, target: self, action: #selector(touchedSideMenuButton))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "list"), style: .plain, target: self, action: #selector(touchedListToggleButton))
+        setNavigationTitle()
+    }
+    
+    private func setNavigationTitle() {
+        let titleButton = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        titleButton.setImage(UIImage(systemName: "circle.grid.2x2"), for: .normal)
+        titleButton.setTitle("WishBall", for: .normal)
+        titleButton.setTitleColor(.black, for: .normal)
+        titleButton.titleLabel?.font = UIFont.nanumExtraBoldFont(ofSize: 17.0)
+        titleButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 11, bottom: 0, right: 0)
+        titleButton.semanticContentAttribute = .forceRightToLeft
+        titleButton.addTarget(self, action: #selector(touchedModifyCollection), for: .touchUpInside)
+        navigationItem.titleView = titleButton
     }
 }
 
